4b5d402c3f8d1c609ea611fc5eaa015ac5e38a55
Luis.DeAndrade <developer-two@relative-engine.com>
2017-08-29 10:49:17 -0500

    comit

--- web-esamyn/src/app/app.router.ts
+++ web-esamyn/src/app/app.router.ts
@@ -5,14 +5,18 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { EstablecimientoSaludComponent } from './establecimiento-salud/establecimiento-salud.component';
 import { AuthGuard } from './guard/index';
 
 export const appRoutes: Routes = [
 	//{ path: '', component: HomeComponent, canActivate: [AuthGuard] },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'home', component: HomeComponent },
-    // otherwise redirect to home
-    { path: '**', redirectTo: '/login' }
+//	{ path: 'login', component: LoginComponent },
+//	{ path: 'home', component: HomeComponent },
+//    // otherwise redirect to home
+//    { path: '**', redirectTo: '/login' }
+	
+	{ path: 'establecimientoSalud', component: EstablecimientoSaludComponent },
+	{ path: '**', redirectTo: '/' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
