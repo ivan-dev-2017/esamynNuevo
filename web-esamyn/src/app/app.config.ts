564583400c6e066d6d56872f2b6782fb4a07a2be
luis.tamayo <luis.tamayo@gmail.com>
2017-09-01 12:57:16 -0500

    REDICRECCION A PRUEBAS http://200.7.213.18

--- web-esamyn/src/app/app.config.ts
+++ web-esamyn/src/app/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     //public readonly apiUrl = 'http://localhost:8080/core-general-web/';
-    public readonly apiUrl = 'http://45.55.147.66:8080/esamyn/';
+    public readonly apiUrl = 'http://200.7.213.18:8080/esamyn/';
     public readonly keyFirstLogin = 'xxxx.xxxx';
     public readonly servicios = {'formulario.crud.findall':'rest/formulario/lista/',
                                   'encuesta.crud.findall':'rest/encuesta/todos',
