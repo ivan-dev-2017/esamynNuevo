cf14969ace7be172d52aed2d1268c9da1f667c20
luis.tamayo <luis.tamayo@gmail.com>
2017-09-01 13:35:44 -0500

    ACTUALIZACION SERVICIOS Y LLAMADA

--- web-esamyn/src/app/service/coreesamyn.service.ts
+++ web-esamyn/src/app/service/coreesamyn.service.ts
@@ -134,8 +134,8 @@ export class CoreesamynService extends BaseService {
 
     getUsuarios(){
       console.log( "==Entra en CoreesamynService getUsuario" );
-      //return this.getAllPaginated(null, 'formulario.crud.findall');
-      return this.http.get('./app/mock/userdata.json').map((response) =>{
+      return this.getAllPaginated(null, 'formulario.crud.findall');
+      /*return this.http.get('./app/mock/userdata.json').map((response) =>{
           let paginatedListx = response.json();
           return paginatedListx.list;
           },
@@ -143,7 +143,7 @@ export class CoreesamynService extends BaseService {
               console.log("==>despues de buscar usuario error  " + JSON.stringify(error));
               return error;
           }
-       );
+       );*/
 
     }
 
@@ -167,4 +167,10 @@ export class CoreesamynService extends BaseService {
           return paginatedListx.objeto;
         });
     }
+    
+    createEncuesta( entidad ){
+        return this.manageWithMessage(entidad, "encuesta.crud.create", "CREACION DE ENCUESTA");
+    }
+    
+    
 }
